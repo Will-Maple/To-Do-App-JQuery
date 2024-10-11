92b8ce2a965178478b17426ae521981ec992f331
Will-Maple17 <will.maple17@gmail.com>
2024-10-11 14:59:27 -0500

    Update scripts.js, styles.css translated over repl into jquery

---
+++ JavaScript/Scripts.js
@@ -0,0 +1,27 @@
+function newItem () {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+    
+
+    if (inputValue === '') {
+        alert("You must write something!");
+      } else {
+        $('#list').append(li);
+    }
+    
+    function strikeout() { 
+        li.toggleClass("strike");
+    }
+    li.on("dblclick", strikeout);
+
+    let button = $('<button class="crossOutButton">X<button>');
+    button.on("click", deleteListItem);
+    li.append(button);
+
+    function deleteListItem(){
+        li.addClass("delete");
+    }
+
+    $('#list').sortable();
+}
